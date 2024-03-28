af0bf77bf82ae40f5a0c97f4872c76dbf5774dab
Rlaal19 <n.prarichaya23@gmail.com>
2024-03-28 22:03:24 +0700

    icon not show

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
 
@@ -12,7 +12,9 @@ function App() {
             <div className='text-sm text-primarycontent font-mono'>23 January 2003</div>
             <div className='text-sm w-5/6 text-primarycontent font-mono'>Study: CHITRALADA TECHNOLOGY INSTITUTE Major: Computer Engineering</div>
             <div className='mt-2'>
-            <span className=' rounded-md bg-primaryTitle text-slate-100 py-2 px-5'>View Resume </span>
+            <span className=' rounded-md bg-primaryTitle text-slate-100 py-2 px-5'>View Resume
+            <FontAwesomeIcon icon="fa-solid fa-file" />
+            </span>
             </div>
           </div>
           <div>Bar</div>
