9476bfadd9f92de203da85aa98f6142ad1843581
Rlaal19 <n.prarichaya23@gmail.com>
2024-03-28 21:59:49 +0700

    can not insert icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
 
 function App() {
 
   return (
-    <div className='grid grid-col-2'>
-      <div>AL</div>
-      <div>92</div>
+    <div className='mt-10 mx-auto max-w-6xl grid grid-cols-[35%_65%]'>
+      <div>
+        <div className='sticky top-14 grid grid-rows-[30%_50%_20%] h-[90vh]'>
+          <div className=' flex flex-col gap-1' >Title
+            <div className='text-3xl text-primaryTitle font-extrabold'>Parichaya Nasomdang</div>
+            <div className='text-sm text-primarycontent font-mono'>23 January 2003</div>
+            <div className='text-sm w-5/6 text-primarycontent font-mono'>Study: CHITRALADA TECHNOLOGY INSTITUTE Major: Computer Engineering</div>
+            <div className='mt-2'>
+            <span className=' rounded-md bg-primaryTitle text-slate-100 py-2 px-5'>View Resume </span>
+            </div>
+          </div>
+          <div>Bar</div>
+          <div className='flex items-end'>contact</div>
+        </div>
+      </div>
+
+      <div>
+        <div>about</div>
+        <div className='mb-96'>experience</div>
+        <div className='mb-96'>experience</div>
+        <div className='mb-96'>experience</div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryTitle: "#7469B6",
+        primarycontent: "#E1AFD1",
+        primarysubtitle: "#AD88C6",
+        primarybase: "#E6A4B4",
+        primarybg: "#FFF8E3",
+      }
+    },
   },
   plugins: [],
 }
